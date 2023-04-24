b229263111a66e60848724f56cb978c64afe1d94
刘吉新 <liujixin87@gmail.com>
2023-04-24 19:43:44 +0800

    [fix] 截屏仅能截到 Flutter 问题

--- build/lib/commands/screen.js
+++ build/lib/commands/screen.js
@@ -2,6 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getScreenshot = void 0;
 const getScreenshot = async function () {
+    if (this.currentContext === 'NATIVE_APP') {
+        return this.proxydriver.getScreenshot();
+    }
     const response = await this.socket.call(`_flutter.screenshot`);
     return response.screenshot;
 };
--- lib/commands/screen.ts
+++ lib/commands/screen.ts
@@ -1,7 +1,7 @@
 import { FlutterDriver } from '../driver';
 
 export const getScreenshot = async function(this: FlutterDriver) {
-  if (this.proxydriverName === 'NATIVE_APP') {
+  if (this.currentContext === 'NATIVE_APP') {
     return this.proxydriver.getScreenshot();
   }
   const response = await this.socket!.call(`_flutter.screenshot`) as any;
